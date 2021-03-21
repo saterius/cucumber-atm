16f733b7d764258882e22398b2a3ad80fbe2067d
saterius <43003809+saterius@users.noreply.github.com>
2021-03-22 00:31:01 +0700

    Add stepDef and deposit feature

--- src/main/java/ku/atm/BankAccount.java
+++ src/main/java/ku/atm/BankAccount.java
@@ -1,52 +1,53 @@
 package ku.atm;
 
 /**
-   A bank account has a balance that can be changed by 
-   deposits and withdrawals.
-*/
+ A bank account has a balance that can be changed by
+ deposits and withdrawals.
+ */
 public class BankAccount {
    private double balance;
 
    /**
-      Constructs a bank account with a zero balance.
-   */
+    Constructs a bank account with a zero balance.
+    */
    public BankAccount() {
       balance = 0;
    }
 
    /**
-      Constructs a bank account with a given balance.
-      @param initialBalance the initial balance
-   */
+    Constructs a bank account with a given balance.
+    @param initialBalance the initial balance
+    */
    public BankAccount(double initialBalance) {
       balance = initialBalance;
    }
- 
-   /** 
-      Deposits money into the account.
-      @param amount the amount of money to withdraw
-   */
+
+   /**
+    Deposits money into the account.
+    @param amount the amount of money to withdraw
+    */
    public void deposit(double amount) {
-      balance = balance + amount;
+      if(amount > 0) {
+         balance = balance + amount;
+      }
    }
 
-   /** 
-      Withdraws money from the account.
-      @param amount the amount of money to deposit
-   */
+   /**
+    Withdraws money from the account.
+    @param amount the amount of money to deposit
+    */
    public void withdraw(double amount) throws NotEnoughBalanceException {
-       if (amount > balance)
+      if (amount > balance)
          throw new NotEnoughBalanceException("cannot withdraw more than balance");
-       balance = balance - amount;
+      balance = balance - amount;
    }
 
-   /** 
-      Gets the account balance.
-      @return the account balance
-   */
+   /**
+    Gets the account balance.
+    @return the account balance
+    */
    public double getBalance() {
-      return balance; 
+      return balance;
    }
 
 }
-
--- src/test/java/ku/atm/StepDefATM.java
+++ src/test/java/ku/atm/StepDefATM.java
@@ -54,6 +54,14 @@ public class StepDefATM {
         assertThrows(NotEnoughBalanceException.class,
                 () -> atm.withdraw(amount));
     }
+
+    @When("I deposit {double} by from ATM")
+    public void i_deposit_by_from_atm(double amount) {
+        // Write code here that turns the phrase above into concrete actions
+        atm.deposit(amount);
+//        throw new io.cucumber.java.PendingException();
+    }
+
     @Then("my account balance is {float}")
     public void my_account_balance_is(double balance) {
         assertEquals(balance, atm.getBalance());
@@ -67,7 +75,7 @@ public class StepDefATM {
     @Then("customer id {int} account balance is {float}")
     public void customer_id_account_balance_is(int id, double balance) {
         assertEquals(balance,
-                     bank.findCustomer(id).getAccount().getBalance());
+                bank.findCustomer(id).getAccount().getBalance());
     }
 
 }
